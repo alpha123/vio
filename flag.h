d644abbd188532450efd15f1bc107ebb98af37a6
peter <peter>
2015-11-17 21:57:34 +0000

    Add license for flag.(c|h) files

--- flag.h
+++ flag.h
@@ -1,6 +1,37 @@
-
 #ifndef _FLAG_H_
 #define _FLAG_H_
+
+/***
+(The MIT License)
+
+Copyright (c) 2015 TJ Holowaychuk <tj@tjholowaychuk.com>
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+***/
+
+/* The above license also applies to flag.c in the same directory. */
+
+/* Modified in minor ways by Peter Cannici. You may find the original
+   versions of these files at:
+   flag.h: https://github.com/clibs/flag/blob/master/flag.h
+   flag.c: https://github.com/clibs/flag/blob/master/flag.c */
 
 #include <stdbool.h>
 
