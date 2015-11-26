db3c3fd0e7d490f5869216019cdf60d55b94e598
peter <peter>
2015-11-26 00:21:32 +0000

    Sift4 string edit distance function.

--- src/stdvio.c
+++ src/stdvio.c
@@ -1,9 +1,30 @@
 #include "math.h"
 #include "strings.h"
+#include "stdrules.h"
 #include "stdvio.h"
 
+/* using _ instead of ._ can make complicated
+   match patterns look much cleaner */
+vio_err_t anyscore(vio_ctx *ctx) {
+    return vio_push_tag(ctx, 1, "_", 0);
+}
+
+vio_err_t vio_drop(vio_ctx *ctx) {
+    if (ctx->sp == 0)
+        return vio_raise_empty_stack(ctx, "drop", 1);
+    --ctx->sp;
+    return 0;
+}
+
 void vio_load_stdlib(vio_ctx *ctx) {
+    vio_load_stdrules(ctx);
+
+    vio_register(ctx, "_", anyscore, 0);
     vio_register(ctx, "++", vio_strcat, 2);
+    vio_register(ctx, "drop", vio_drop, 0);
+
+    vio_register(ctx, "edit-dist", vio_edit_dist, 2);
+    vio_register(ctx, "str-sim", vio_approx_edit_dist, 2);
 
 #define REGISTER(f) vio_register(ctx, #f, vio_##f, 1);
     LIST_MATH_UNARY(REGISTER)
--- src/strings.c
+++ src/strings.c
@@ -1,6 +1,114 @@
+#if defined(__unix__)
+#include <sys/param.h>
+#ifdef BSD
+#include <stdlib.h>
+#else
+#include <alloca.h>
+#endif
+#elif defined(_WIN32)
+#include <malloc.h>
+#define alloca _alloca
+#endif
+
+#include <math.h>
 #include "divsufsort.h"
 #include "mpc.h"
 #include "strings.h"
+
+/* 5 is chosen rather arbitrarily to avoid moving on every
+   loop. Larger values will be faster but less accurate.
+   Generally increasing it would only be useful if you are
+   comparing long strings with many sequences of equal characters. */
+#define VIO_SIFT4_OFF_SHIFT 5
+
+/* See http://siderite.blogspot.com/2014/11/super-fast-and-accurate-string-distance.html
+
+   Don't use really large values for maxoff.
+
+   If maxdist > -1, stop once we hit maxdist differences. */
+uint32_t vio_sift4_dist(uint32_t alen, const char *a, uint32_t blen, const char *b, int maxoff, int maxdist) {
+    if (alen == 0) return blen;
+    if (blen == 0) return alen;
+
+    uint32_t ia = 0, ib = 0, lcss = 0, local_cs = 0, trans_cnt = 0,
+             olen = 0,
+             *off_a = (uint32_t *)alloca(maxoff),
+             *off_b = (uint32_t *)alloca(maxoff);
+    uint8_t *off_test = (uint8_t *)alloca(maxoff);
+
+    while (ia < alen && ib < blen) {
+        if (a[ia] == b[ib]) {
+            ++local_cs;
+            /* Check for transpositions. Not fully accurate, as it will only look
+               at a section of the string at most `maxoff` long. */
+            uint8_t transp = 0;
+            for (uint32_t i = 0; i < olen;) {
+                uint32_t oa = off_a[i], ob = off_b[i];
+                uint8_t test = off_test[i];
+                if (ia < oa && ib < ob) {
+                    transp = abs(ib - ia) >= abs(ob - oa);
+                    if (transp)
+                        ++trans_cnt;
+                    else if (!test) {
+                        off_test[i] = 1;
+                        ++trans_cnt;
+                    }
+                    break;
+                }
+                if (ia > ob && ib > oa) {
+                    memmove(off_a + i, off_a + i + 1, 1);
+                    memmove(off_b + i, off_b + i + 1, 1);
+                    memmove(off_test + i, off_test + i + 1, 1);
+                    --olen;
+                }
+                else
+                    ++i;
+            }
+            if (olen == maxoff) {
+                memmove(off_a, off_a + VIO_SIFT4_OFF_SHIFT, VIO_SIFT4_OFF_SHIFT);
+                memmove(off_b, off_b + VIO_SIFT4_OFF_SHIFT, VIO_SIFT4_OFF_SHIFT);
+                memmove(off_test, off_test + VIO_SIFT4_OFF_SHIFT, VIO_SIFT4_OFF_SHIFT);
+                olen -= VIO_SIFT4_OFF_SHIFT;
+            }
+            off_a[olen] = ia;
+            off_b[olen] = ib;
+            off_test[olen++] = transp;
+        }
+        else {
+            lcss += local_cs;
+            local_cs = 0;
+            if (ia > ib) ia = ib;
+            else ib = ia;
+            for (uint32_t i = 0; i < maxoff && (ia + i < alen || ib + i < blen); ++i) {
+                if (ia + i < alen && a[ia + i] == b[ib]) {
+                    ia += i - 1;
+                    --ib;
+                    break;
+                }
+                else if (ib + i < blen && b[ib + i] == a[ia]) {
+                    ib += i - 1;
+                    --ia;
+                    break;
+                }
+            }
+        }
+        ++ia;
+        ++ib;
+        if (maxdist > -1) {
+            uint32_t check = (ia > ib ? ia : ib) - lcss + trans_cnt;
+            if (check >= maxdist)
+                return check;
+        }
+        if (ia >= alen || ib >= blen) {
+            lcss += local_cs;
+            local_cs = 0;
+            if (ia > ib) ia = ib;
+            else ib = ia;
+        }
+    }
+    lcss += local_cs;
+    return (alen > blen ? alen : blen) - lcss + trans_cnt;
+}
 
 vio_err_t vio_strcat(vio_ctx *ctx) {
     vio_err_t err = 0;
@@ -31,6 +139,19 @@ vio_err_t vio_strsplit(vio_ctx *ctx) {
 
 vio_err_t vio_edit_dist(vio_ctx *ctx) {
     return 0;
+}
+
+vio_err_t vio_approx_edit_dist(vio_ctx *ctx) {
+    vio_err_t err = 0;
+    uint32_t alen, blen;
+    char *a, *b;
+    VIO__CHECK(vio_pop_str(ctx, &alen, &a));
+    VIO__CHECK(vio_pop_str(ctx, &blen, &b));
+    VIO__CHECK(vio_push_int(ctx, vio_sift4_dist(alen, a, blen, b, 10, -1)));
+
+    return 0;
+    error:
+    return err;
 }
 
 vio_err_t vio_sorted_sufarray(vio_ctx *ctx) {
